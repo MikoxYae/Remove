49ced9a791bc48a37b7f4b4e08e034bd1a7ee1b6
Yae Miko <aloksinghkumar756@gmail.com>
2025-10-23 23:04:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ API_HASH = os.environ.get("API_HASH", "c2e33826d757fe113bc154fcfabc987d")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8337038537:AAH-Ej6TORVv1eBx11aFF3bKKJKNGb5x7M8")
 OWNER_ID = int(os.environ.get("OWNER_ID", "7534267467"))
 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Angel:aloksingh@angel.4qdpllb.mongodb.net/?retryWrites=true&w=majority&appName=Angel")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Test:aloksingh@cluster0.iomykdc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Angle")
